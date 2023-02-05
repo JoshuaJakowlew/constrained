7ffa87ac8e88de3053c9f3c5486e76598b50f65b
Яковлев Георгий <darksmolensksky@gmail.com>
2023-02-05 03:49:17 +0300

    Refactor macro code generator Add algorithm checks

--- include/constrained_type/combinators.hpp
+++ include/constrained_type/combinators.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 #include <constrained_type/combinators/static/relational.hpp>
+#include <constrained_type/combinators/static/algorithm.hpp>
--- include/constrained_type/combinators/static/relational.hpp
+++ include/constrained_type/combinators/static/relational.hpp
@@ -1,85 +1,41 @@
 #pragma once
 
-#include <concepts>
-#include <type_traits>
+#include <constrained_type/combinators/static/macro.hpp>
 
-#include <constrained_type/constrained_type.hpp>
+#define CONSTRAINED_TYPE_RELATIONAL() \
+    CONSTRAINED_TYPE_BIN_OP(eq,  ==) \
+    CONSTRAINED_TYPE_BIN_OP(neq, !=) \
+    CONSTRAINED_TYPE_BIN_OP(gt,   >) \
+    CONSTRAINED_TYPE_BIN_OP(ge,  >=) \
+    CONSTRAINED_TYPE_BIN_OP(lt,   <) \
+    CONSTRAINED_TYPE_BIN_OP(le,  <=)
 
-#pragma region by_value
-#define CONSTRAINED_TYPE_RELATIONAL_BY_VALUE(name, op) \
-    template <auto Value> \
-    struct name \
-    { \
-        [[nodiscard]] constexpr bool operator()(auto const & x) const \
-            noexcept(noexcept(x op static_cast<std::remove_reference_t<decltype(x)>>(Value))) \
-            requires std::convertible_to<decltype(Value), std::remove_reference_t<decltype(x)>> \
-        { \
-            return x op static_cast<std::remove_reference_t<decltype(x)>>(Value); \
-        } \
-    };
+#define CONSTRAINED_TYPE_RELATIONAL_BY_METHOD(method) \
+    CONSTRAINED_TYPE_BIN_OP_BY_METHOD(method, eq,  ==) \
+    CONSTRAINED_TYPE_BIN_OP_BY_METHOD(method, neq, !=) \
+    CONSTRAINED_TYPE_BIN_OP_BY_METHOD(method, gt,   >) \
+    CONSTRAINED_TYPE_BIN_OP_BY_METHOD(method, ge,  >=) \
+    CONSTRAINED_TYPE_BIN_OP_BY_METHOD(method, lt,   <) \
+    CONSTRAINED_TYPE_BIN_OP_BY_METHOD(method, le,  <=)
 
-#define CONSTRAINED_TYPE_RELATION_BY_VALUE() \
-    CONSTRAINED_TYPE_RELATIONAL_BY_VALUE(eq,  ==) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_VALUE(neq, !=) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_VALUE(gt,   >) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_VALUE(ge,  >=) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_VALUE(lt,   <) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_VALUE(le,  <=)
-#pragma endregion by_value
-#pragma region by_method
-#define CONSTRAINED_TYPE_RELATION_BY_METHOD_SIZE_T(method) \
-    namespace detail::method_##method \
-    { \
-        template <typename T> \
-        using size_t = decltype(std::declval<T>().method()); \
-    }
-
-// Currently decltype(x) is const reference, so for std::string and size method
-// detail::method_size::size_t will be parametrized by const std::string &, not by std::string
-// This doesn't affect type uniqueness and simplifies compiler work (e.g. no std::remove_reference_t)
-#define CONSTRAINED_TYPE_RELATIONAL_BY_METHOD(prefix, postfix, op, method) \
-    template <auto Value> \
-    struct prefix##_##postfix \
-    { \
-        [[nodiscard]] constexpr bool operator()(auto const & x) const \
-            noexcept(noexcept(x.method() op static_cast<detail::method_##method::size_t<decltype(x)>>(Value))) \
-            requires std::convertible_to<decltype(Value), detail::method_##method::size_t<decltype(x)>> \
-        { \
-            return x.method() op static_cast<detail::method_##method::size_t<decltype(x)>>(Value); \
-        } \
-    }; \
-
-#define CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_POSTFIX(postfix, op, method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD(method, postfix, op, method)
-
-#define CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_PREFIX(prefix, op, method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD(prefix, method, op, method)
-
-#define CONSTRAINED_TYPE_OP_RELATION_BY_METHOD(method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_POSTFIX(eq,  ==, method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_POSTFIX(neq, !=, method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_POSTFIX(gt,   >, method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_POSTFIX(ge,  >=, method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_POSTFIX(lt,   <, method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_POSTFIX(le,  <=, method)
-
-#define CONSTRAINED_TYPE_MINMAX_RELATION_BY_METHOD(method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_PREFIX(min,  >=, method) \
-    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_PREFIX(max,  <=, method)
-
-#define CONSTRAINED_TYPE_RELATION_BY_METHOD(method) \
-    CONSTRAINED_TYPE_RELATION_BY_METHOD_SIZE_T(method) \
-    CONSTRAINED_TYPE_OP_RELATION_BY_METHOD(method) \
-    CONSTRAINED_TYPE_MINMAX_RELATION_BY_METHOD(method)
-#pragma endregion by_method
+#define CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_MIN_MAX_ALIAS(method) \
+    CONSTRAINED_TYPE_ALIAS(min_##method, method##_ge) \
+    CONSTRAINED_TYPE_ALIAS(max_##method, method##_le)
 
 namespace ct
 {
-    CONSTRAINED_TYPE_RELATION_BY_VALUE();
+    // Comparisons for <value> <op> <static_param>
+    CONSTRAINED_TYPE_RELATIONAL();
 
-    CONSTRAINED_TYPE_RELATION_BY_METHOD(size    );
-    CONSTRAINED_TYPE_RELATION_BY_METHOD(length  );
-    CONSTRAINED_TYPE_RELATION_BY_METHOD(capacity);
-    CONSTRAINED_TYPE_RELATION_BY_METHOD(front   );
-    CONSTRAINED_TYPE_RELATION_BY_METHOD(back    );
+    // Comparisons for <value.<method>()> <op> <static_param>
+    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD(size    );
+    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD(length  );
+    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD(capacity);
+
+    // Aliases:
+    // min_<method> ~ <method>_ge
+    // max_<method> ~ <method>_le
+    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_MIN_MAX_ALIAS(size    )
+    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_MIN_MAX_ALIAS(length  )
+    CONSTRAINED_TYPE_RELATIONAL_BY_METHOD_MIN_MAX_ALIAS(capacity)
 } //namespace ct
